c63106f8f24acb3093ce1f22183ada86946dc79b
Anastas Dancha <anapsix@random.io>
2015-05-20 16:38:16 -0400

    TUTUM: remove netmask from IP

--- trusty/1.15.1/docker-entrypoint.sh
+++ trusty/1.15.1/docker-entrypoint.sh
@@ -5,7 +5,7 @@ TUTUM_ARGS=()
 
 if [ "${1:0:1}" = '-' ]; then
   [ -n "$TUTUM_CONTAINER_FQDN" ] && TUTUM_ARGS+=( --canonical-address $TUTUM_CONTAINER_FQDN )
-  [ -n "$TUTUM_IP_ADDRESS" ] && TUTUM_ARGS+=( --canonical-address $TUTUM_IP_ADDRESS )
+  [ -n "$TUTUM_IP_ADDRESS" ] && TUTUM_ARGS+=( --canonical-address ${TUTUM_IP_ADDRESS%/*} )
   [ -n "$TUTUM_SERVICE_FQDN" ] && TUTUM_ARGS+=( --join $TUTUM_SERVICE_FQDN )
   set -- rethinkdb "$@" "${TUTUM_ARGS[@]}"
 fi
