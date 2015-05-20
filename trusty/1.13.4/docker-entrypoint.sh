dab2948436059f56d3635f8aabb3621e154f21b4
Anastas Dancha <anapsix@random.io>
2015-05-20 16:12:19 -0400

    TUTUM: compatible start script

--- trusty/1.13.4/docker-entrypoint.sh
+++ trusty/1.13.4/docker-entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+TUTUM_ARGS=()
+
 if [ "${1:0:1}" = '-' ]; then
-  set -- rethinkdb "$@"
+  [ -n "$TUTUM_CONTAINER_FQDN" ] && TUTUM_ARGS+=( --canonical-address $TUTUM_CONTAINER_FQDN )
+  [ -n "$TUTUM_IP_ADDRESS" ] && TUTUM_ARGS+=( --canonical-address $TUTUM_IP_ADDRESS )
+  [ -n "$TUTUM_SERVICE_FQDN" ] && TUTUM_ARGS+=( --join $TUTUM_SERVICE_FQDN )
+  set -- rethinkdb "$@" "${TUTUM_ARGS[@]}"
 fi
 
 exec "$@"
