d88fb19d9f862717e099a8ea0bc646908c409857
Anastas Dancha <anapsix@random.io>
2015-06-10 09:39:39 -0400

    FIXUP/CLEANUP: fix generation script, cleanup

--- generate_dockerfile.sh
+++ generate_dockerfile.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 # re-generate Dockerfile for requested distribution(s)
 
-dists=( ubuntu debian )
-releases=( jessie trusty utopic wheezy )
+dists=( ubuntu:trusty ubuntu:utopic debian:wheezy debian:jessie )
 versions=( 1.15.1 1.15.2 1.15.3 1.16.0 1.16.1 1.16.2 1.16.3 2.0.0 2.0.1 2.0.2 )
 
 DOCKERFILE_TEMPLATE=Dockerfile.tpl
 
 for dist in ${dists[@]}; do
-  test -d $dist || mkdir $dist
-  for release in ${releases[@]}; do
-    test -d $dist/$release || mkdir $dist/$release
-    for version in ${versions[@]}; do
-      test -d $dist/$release/$version || mkdir $dist/$release/$version
-      sed "/^ENV RETHINKDB_PACKAGE_VERSION/s/%version%/${version}/g;
-           /^FROM/s/%dist%/${dist}/g;
-           s/%release%/${release}/g" $DOCKERFILE_TEMPLATE > $dist/$release/$version/Dockerfile
-      cp ./docker-entrypoint.sh $dist/$release/$version/
-    done
+  vendor=${dist%:*}
+  release=${dist#*:}
+  test -d $vendor || mkdir $vendor
+  test -d $vendor/$release || mkdir $vendor/$release
+  for version in ${versions[@]}; do
+    test -d $vendor/$release/$version || mkdir $vendor/$release/$version
+    sed "/^ENV RETHINKDB_PACKAGE_VERSION/s/%version%/${version}/g;
+         /^FROM/s/%dist%/${vendor}/g;
+         s/%release%/${release}/g" $DOCKERFILE_TEMPLATE > $vendor/$release/$version/Dockerfile
+    cp ./docker-entrypoint.sh $vendor/$release/$version/
   done
 done
