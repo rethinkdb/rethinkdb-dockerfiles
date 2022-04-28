4f89aed1598e2851ad54cf2c379f992107076a0c
Gabor Boros <gabor.brs@gmail.com>
2022-04-28 16:04:58 +0200

    Add 2.4.2
    Signed-off-by: Gabor Boros <gabor.brs@gmail.com>

--- cut-new-release.sh
+++ cut-new-release.sh
@@ -1,23 +1,22 @@
 #! /usr/bin/env bash
 
-BASE_VERSION=2.4.0
+BASE_VERSION=2.4.2
 NEW_VERSION=$1
 SUFFIX=$2
 
 # packages for them to downloads.rethinkdb.com
-DISTROS="focal xenial bionic disco eoan centos7 centos8 jessie stretch buster trusty"
+DISTROS="alma8 bionic bullseye buster centos7 focal hirsute impish jammy jessie rocky8 stretch trusty xenial"
 
 function bootstrap_and_build {
   for DISTRO in $DISTROS; do
-    if [ -d "$DISTRO/$NEW_VERSION" ]; then 
+    if [ -d "$DISTRO/$NEW_VERSION" ]; then
       echo "$DISTRO/$NEW_VERSION already exists... Skipping Dockerfile bootstrap"
     elif [ -f "$DISTRO/$BASE_VERSION/Dockerfile" ]; then
       mkdir "./$DISTRO/$NEW_VERSION"
-      sed -e "s/$BASE_VERSION/$NEW_VERSION$SUFFIX/" "./$DISTRO/$BASE_VERSION/Dockerfile" \
-        >"./$DISTRO/$NEW_VERSION/Dockerfile"
+      sed -e "s/$BASE_VERSION/$NEW_VERSION$SUFFIX/" "./$DISTRO/$BASE_VERSION/Dockerfile" > "./$DISTRO/$NEW_VERSION/Dockerfile"
     fi
 
-    docker build --no-cache -t rethinkdb:$DISTRO-$NEW_VERSION$SUFFIX $DISTRO/$NEW_VERSION$SUFFIX
+    docker build --no-cache -t "rethinkdb:$DISTRO-$NEW_VERSION$SUFFIX" "$DISTRO/$NEW_VERSION$SUFFIX"
   done
 }
 
@@ -28,9 +27,9 @@ function commit_and_tag {
 }
 
 if [[ -z "$1" ]]; then
-  echo "cut-NEW_VERSION-release: tag not specified" >&2
+  echo "cut-new-release: tag not specified" >&2
   exit 1
 fi
 
-bootstrap_and_build;
+# bootstrap_and_build;
 commit_and_tag;
